27c076c8750459a35d71c99ad0ba2a4842dbc798
Eugene Cheipesh <echeipesh@gmail.com>
2012-12-07 20:55:21 -0500

    Problem 16

--- src/main/scala/P15.scala
+++ src/main/scala/P15.scala
@@ -3,23 +3,13 @@ object P15 {
 	val maxX = 20;
 	val maxY = 20;
 
-	case class Pos(x: Int, y: Int){
-		def neighbors() = 
-			List(Pos(x+1, y), Pos(x, y+1)).filter(_.isValid)
 
-		def isValid = {x >= 0 && y >= 0 && x<=maxY && y<=maxY}
-
-		def isFinished = x == maxX && y == maxY
-	}
-
-	def explore(pos:Pos, explored: Set[Pos]):Long = {
-		if (pos.isFinished){
+	def explore(x:Int, y:Int):Long = 
+		if (x == maxX && y == maxY)
 			1
-		}else{
-			(for (n <- pos.neighbors if ! explored.contains(n)) yield {explore(n, explored + pos)}).sum
-		}
-	}
+		else
+			(if (x < maxX) explore(x+1, y) else 0) + (if (y < maxY) explore(x,y+1) else 0)
 
-	//code seems simple but this was a pain to write/debug. Not enough intermediate steps I think.
-	lazy val answer = explore(Pos(0,0), Set.empty)
+	lazy val answer = explore(0,0)
+	//this is not the best way to do it, takes FOREVER
 }
